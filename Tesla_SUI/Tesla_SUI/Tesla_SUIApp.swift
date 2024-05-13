cce4b28c47ccdde9c29761afbfd47ad8067e4090
Yevgeny Ly <globchenko16@gmail.com>
2024-05-13 16:33:05 +0300

    - app init

---
+++ Tesla_SUI/Tesla_SUI/Tesla_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tesla_SUIApp.swift
+//  Tesla_SUI
+//
+//  Created by Евгений Л on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tesla_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
